4e21eabab54d5ba5c81fc8d1c155d1e38150e0ff
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-21 23:18:37 -0400

    Add files via upload

---
+++ reversestring.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+    // step one. Using the split() method we take our current string and split each letter into it's on seperate 
+    //"sub string"!
+    //step two. We take all our sub string using the reverse() method to reverse the order of them.
+    //Step three. We use the join() method to put our reversed string back together into a single string.
+    // Last, we use return to take the string we would like to reverse using this function and it then
+    //outputs that to console.log!....or you could just use return if were not using a javascript
+    // and this was in a whiteboard interview. GG
+}
+return "hello";
+
+//fastest way I've figured out to reverse a string.
